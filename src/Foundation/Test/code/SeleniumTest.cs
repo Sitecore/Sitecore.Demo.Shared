6f862538846996091c71fbe5366feb46891050e0
Jean-François Larente <jfl@sitecore.net>
2019-07-04 05:26:29 -0400

    Updated incorrect namespace

--- src/Foundation/Test/code/SeleniumTest.cs
+++ src/Foundation/Test/code/SeleniumTest.cs
@@ -12,7 +12,7 @@ using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Interactions;
 using OpenQA.Selenium.Support.UI;
 
-namespace Sitecore.Demo.Shared.Test
+namespace Sitecore.Demo.Foundation.Test
 {
     public class SeleniumTest
     {
